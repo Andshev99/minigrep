bc802a5bea080c19bc1ce0db2f80fa4507a2e9f2
shevtsov_ajax <shevtsov@ajax.systems>
2022-10-09 21:39:45 +0300

    Завершение работы с кодом ошибки

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs;
+use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     // println!("{:?}", args);
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|e| {
+        println!("Проблема при разборе аргументов: {}", e);
+        process::exit(1);
+    });
 
     println!("Поиск {}", config.query);
     println!("В файле {}", config.filename);
@@ -18,9 +22,12 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("недостаточно аргументов");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
