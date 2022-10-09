9cd83431190a8e58885e726270d84b78b2ddd891
shevtsov_ajax <shevtsov@ajax.systems>
2022-10-09 21:01:58 +0300

    add Config

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,22 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     // println!("{:?}", args);
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Поиск {}", query);
-    println!("В файле {}", filename);
-    let contents = fs::read_to_string(filename).expect("Что-то пошло не так при чтении файла");
+    let config = parse_config(&args);
+
+    println!("Поиск {}", config.query);
+    println!("В файле {}", config.filename);
+    let contents =
+        fs::read_to_string(config.filename).expect("Что-то пошло не так при чтении файла");
     println!("С текстом:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Config { query, filename }
 }
