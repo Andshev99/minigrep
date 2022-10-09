9c77904e4b47df3728fc7be7e598ce784f2d5dca
shevtsov_ajax <shevtsov@ajax.systems>
2022-10-09 19:46:55 +0300

    чтение файла

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use std::env;
+use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    // println!("{:?}", args);
+    let query = &args[1];
+    let filename = &args[2];
+    println!("Поиск {}", query);
+    println!("В файле {}", filename);
+    let contents = fs::read_to_string(filename).expect("Что-то пошло не так при чтении файла");
+    println!("С текстом:\n{}", contents);
 }
