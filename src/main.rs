f12413209bd5da9880ed9b1284d18678a891796c
shevtsov_ajax <shevtsov@ajax.systems>
2022-10-10 13:13:22 +0300

    add search and

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Поиск {}", config.query);
-    println!("В файле {}", config.filename);
+    // println!("Поиск {}", config.query);
+    // println!("В файле {}", config.filename);
     if let Err(e) = minigrep::run(config) {
         println!("Ошибка в приложении: {}", e);
         process::exit(1);
