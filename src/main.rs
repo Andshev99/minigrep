6b1483e39356a9455545fcbfbcfb62b85f892383
shevtsov_ajax <shevtsov@ajax.systems>
2022-10-10 15:10:16 +0300

    вывод ошибок в поток ошибок

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     // println!("{:?}", args);
     let config = Config::new(&args).unwrap_or_else(|e| {
-        println!("Проблема при разборе аргументов: {}", e);
+        eprintln!("Проблема при разборе аргументов: {}", e);
         process::exit(1);
     });
 
     // println!("Поиск {}", config.query);
     // println!("В файле {}", config.filename);
     if let Err(e) = minigrep::run(config) {
-        println!("Ошибка в приложении: {}", e);
+        eprintln!("Ошибка в приложении: {}", e);
         process::exit(1);
     }
 }
